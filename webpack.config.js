554d8a9d2c1dc049886371cc6beea514b90ffc17
sanjay <sanjay@live.com>
2017-08-09 19:18:44 +0530

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js"
+  },
+  module: {
+     rules: [
+       {
+         test: /\.css$/,
+         use: [
+           'style-loader',
+           'css-loader'
+         ]
+       },
+       {
+         test: /\.(png|svg|jpg|gif)$/,
+         use: [
+           'file-loader'
+         ]
+       }
+     ]
+   }
+}
